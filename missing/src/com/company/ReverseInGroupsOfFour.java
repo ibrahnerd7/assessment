b0ee452b348f164ff868123e5879150235b7f939
ibrahnerd7 <ibrahmaina7@gmail.com>
2020-10-19 13:11:09 +0300

    Create reverse method in reverse group class

--- missing/src/com/company/ReverseInGroupsOfFour.java
+++ missing/src/com/company/ReverseInGroupsOfFour.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class ReverseInGroupsOfFour {
+    public String reverseInGroupsOfFour(String string) {
+        String reversed = "";
+        String regex = "(?<=\\G.{4})";
+        String[] stringGroups = string.split(regex);
+
+        for (String stringSubArray : stringGroups) {
+            StringBuilder stringBuilder = new StringBuilder(stringSubArray);
+            reversed = stringBuilder.reverse().toString();
+        }
+
+        return reversed;
+    }
 }
