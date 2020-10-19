a5c1756c3d531d7e41937433affe619f0a90a76e
ibrahnerd7 <ibrahmaina7@gmail.com>
2020-10-19 13:48:51 +0300

    Format database script file

--- missing/src/com/company/thirdproblem/education.sql
+++ missing/src/com/company/thirdproblem/education.sql
@@ -1,24 +1,24 @@
 CREATE database education;
 
 CREATE TABLE institution (
-institutionid INT(11) NOT NULL AUTO_INCREMENT,
-name VARCHAR(250),
-PRIMARY KEY(institutionid)
+                             institutionid INT(11) NOT NULL AUTO_INCREMENT,
+                             name VARCHAR(250),
+                             PRIMARY KEY(institutionid)
 );
 
 CREATE TABLE course(
-courseid INT(11) NOT NULL AUTO_INCREMENT,
-name VARCHAR(250),
-institution INT(11),
-PRIMARY KEY(courseid),
-FOREIGN KEY(institutionid) REFERENCES institution(institutionid)
+                       courseid INT(11) NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(250),
+                       institution INT(11),
+                       PRIMARY KEY(courseid),
+                       FOREIGN KEY(institution) REFERENCES institution(institutionid)
 );
 
 CREATE TABLE student(
-studentid INT(11) NOT NULL AUTO_INCREMENT,
-name VARCHAR(250),
-courseid INT(11),
-PRIMARY KEY(studentid),
-FOREIGN KEY(courseid) REFERENCES course(courseid)
+                        studentid INT(11) NOT NULL AUTO_INCREMENT,
+                        name VARCHAR(250),
+                        course INT(11),
+                        PRIMARY KEY(studentid),
+                        FOREIGN KEY(course) REFERENCES course(courseid)
 );
 
