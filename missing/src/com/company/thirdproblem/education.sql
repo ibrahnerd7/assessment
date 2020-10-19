1ff6b96b54480a310afc87abf4f2ccfae586d2a0
ibrahnerd7 <ibrahmaina7@gmail.com>
2020-10-19 13:43:25 +0300

    Create course table

--- missing/src/com/company/thirdproblem/education.sql
+++ missing/src/com/company/thirdproblem/education.sql
@@ -2,6 +2,15 @@ CREATE database education;
 
 CREATE TABLE institution (
 institutionid INT(11) NOT NULL AUTO_INCREMENT,
-institutionname VARCHAR(250),
+name VARCHAR(250),
 PRIMARY KEY(institutionid)
-)
+);
+
+CREATE TABLE course(
+courseid INT(11) NOT NULL AUTO_INCREMENT,
+name VARCHAR(250),
+institution INT(11),
+PRIMARY KEY(courseid),
+FOREIGN KEY(institutionid) REFERENCES institution(institutionid)
+);
+
